d467f815956e59de8178c60c20e6852ca05e8f61
Svitlana Khmarska <svitlana.khmarska@gmail.com>
2017-04-14 08:30:45 +0300

    lab4
    blablabla

---
+++ source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::endl;
+using std::cout;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+
+class word {
+private:
+	string text;
+	double num_X;	// Кількість слів у реченні
+	double num_Y;	// Кількість символів у реченні
+public:
+	word(){
+		text = "";
+		num_X = 0.0;
+		num_Y = 0.0;
+	}
+	double get_X(){return num_X;}
+	double get_Y(){return num_Y;}
+	string get_text(){return text;}
+	void set_X(double X){num_X = X;}
+	void set_Y(double Y){num_Y = Y;}
+	void set_text(string str){text.assign(str);}
+};
+
+vector<word> dictionary;
+
+string filename;
+void parseSentence();
+int parseFile();
+
+int main(){
+
+	parseFile();
+	return 0;
+}
